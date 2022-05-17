7df2da189b43b2dedadb330d25313748b4de8a9d
hwj <jminie@naver.com>
2022-05-17 21:28:42 +0900

    2주차

---
+++ 프로그래머스/카펫.py
@@ -0,0 +1,9 @@
+def solution(brown, yellow):
+    answer = []
+    s = brown + yellow
+    for i in range(s, 2, -1):
+        if s % i == 0:
+            a = s // i
+            if yellow == (i - 2) * (a - 2):
+                return [i, a]
+    return answer
