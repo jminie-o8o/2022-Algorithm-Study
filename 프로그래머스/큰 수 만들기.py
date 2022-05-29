32a9060053eccc4ae681fd92211e6483ff83deee
hwj <jminie@naver.com>
2022-05-29 19:50:37 +0900

    3주차 2번, 3번

---
+++ 프로그래머스/큰 수 만들기.py
@@ -0,0 +1,13 @@
+def solution(number, k):
+    answer = ''
+    number_list = list(number)
+    stack = []
+    for number in number_list:
+        while stack and stack[-1] < number and k > 0:
+            stack.pop()
+            k -= 1
+        stack.append(number)
+    if k > 0:
+        stack = stack[:-k]
+    answer = ''.join(stack)
+    return answer
